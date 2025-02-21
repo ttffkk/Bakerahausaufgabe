82a165b440aac2eec7614da31ff645fe739feab0
Tim <tfkaminski@gmail.com>
2025-02-21 08:51:40 +0100

    t

---
+++ src/main/java/de/tk/bakerahausaufgabe/BakerahausaufgabeApplication.java
@@ -0,0 +1,13 @@
+package de.tk.bakerahausaufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerahausaufgabeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakerahausaufgabeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/tk/bakerahausaufgabe/BakerahausaufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tk.bakerahausaufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerahausaufgabeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
